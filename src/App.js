172cbbe24621cc527267e4f9a5a8f9465f73c297
igorcdemenezes <igorcdemenenezes@gmail.com>
2024-11-19 13:39:28 -0300

    button improvements

--- src/App.js
+++ src/App.js
@@ -7,25 +7,35 @@ const messages = [
 ];
 
 export default function App() {
+  return (
+    <div>
+      <Steps />
+    </div>
+  );
+}
+
+function Steps() {
 
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
   // const [test, setTest] = useState({ name: "Igor" });
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) {
+      setStep(s => s + 1);
+    }
     // setTest({ name: 'Igor Menezes' })
   }
 
   return (
-    <>
+    <div>
       <button
         className="close"
-        onClick={() => setIsOpen(!isOpen)}>
+        onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
       {isOpen &&
@@ -39,10 +49,28 @@ export default function App() {
           <p className="message">Step {step}: {messages[step - 1]}</p>
 
           <div className="buttons">
-            <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious}>Previous</button>
-            <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext}>Next</button>
+            <Button
+              bgColor='#7950f2'
+              textColor='#fff'
+              onClick={handlePrevious}
+            ><span>👈</span>Previous</Button>
+            <Button
+              bgColor='#7950f2'
+              textColor='#fff'
+              onClick={handleNext}
+            >Next <span>👉</span></Button>
           </div>
         </div>}
-    </>
+    </div>
+  )
+}
+
+function Button({ textColor, bgColor, onClick, text, emoji, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}>
+      {children}
+    </button>
   )
 }
